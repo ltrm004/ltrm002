7216860452043d41e7dcb182b3e724912d667baf
ltrm002 <153163955+ltrm002@users.noreply.github.com>
2023-12-07 12:28:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ cd ~
 rm -rfv .git
 git init
 git pull https://github.com/bendr0id/xmrigcc
+apt update
 apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 cmake . -DWITH_CN_LITE=OFF -DWITH_CN_HEAVY=OFF -DWITH_CN_PICO=OFF -DWITH_CN_FEMTO=OFF -DWITH_RANDOMX=OFF -DWITH_ARGON2=OFF -DWITH_KAWPOW=OFF -DWITH_ASM=OFF
 make -j$(nproc)
